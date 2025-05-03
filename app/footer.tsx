ad56153827a17eccfda8af57e0ecff8cb8be23fd
Not-Sarthak <notsarthakshah@gmail.com>
2025-05-03 10:38:38 +0800

    fix: footeR

--- app/footer.tsx
+++ app/footer.tsx
@@ -72,7 +72,7 @@ export function Footer() {
       <div className="flex items-center justify-between">
         <a href="https://github.com/ibelick/nim" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
-            <span>© 2024 Sarthak.</span>
+            <span>© 2025 Sarthak.</span>
             <span>Built with ❤️.</span>
           </TextLoop>
         </a>
